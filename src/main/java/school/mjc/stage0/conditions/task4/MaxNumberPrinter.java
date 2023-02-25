494659190a0ba71c6b551ac79bcbb7a01bf472d8
wickerman2019 <sverige@i.ua>
2023-02-25 12:25:53 +0200

    Ternary operator task

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max;
+        if (first > second) max = first;
+        else if (second > third) max = second;
+        else max = third;
+        System.out.println(max);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        int bigger = (first>second)?first:second;
+        System.out.println(bigger);
     }
 }
